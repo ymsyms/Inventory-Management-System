bea786990f8b9540471de2ac2d172ed2240fad5c
Yimon Soe <Yimon Soe@172.17.250.219>
2017-12-07 14:06:47 +0800

    This is Start up project for team 5 Inventory System.

---
+++ Team5_InventorySystem/src/main/java/sg/edu/iss/inventory/Team5InventorySystemApplication.java
@@ -0,0 +1,12 @@
+package sg.edu.iss.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5InventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5InventorySystemApplication.class, args);
+	}
+}
