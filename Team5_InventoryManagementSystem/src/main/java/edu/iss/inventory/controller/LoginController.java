959601684793db7a76482c4d4368596e40a0ad9a
ymsyms <yimonsoe.yms@gmail.com>
2017-12-08 11:27:38 +0800

    [Added service User]

--- Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/LoginController.java
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/LoginController.java
@@ -1,10 +1,16 @@
 package edu.iss.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.iss.inventory.service.UserService;
 
 @Controller
 @RequestMapping(value = "/login")
 public class LoginController {
+	
+	@Autowired
+	UserService userService;
 
 }
--- Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/service/UserServiceImpl.java
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/service/UserServiceImpl.java
@@ -1,5 +1,13 @@
 package edu.iss.inventory.service;
 
+import javax.annotation.Resource;
+
+import edu.iss.inventory.repository.UserRepository;
+
 public class UserServiceImpl implements UserService{
 
+	@Resource
+	UserRepository userRepository;
+	
+	
 }
