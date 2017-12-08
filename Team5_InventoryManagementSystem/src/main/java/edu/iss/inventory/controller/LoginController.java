66cf90288fa30ffd7c20ed3ca12d3d6a1fe046e1
ymsyms <yimonsoe.yms@gmail.com>
2017-12-08 12:02:33 +0800

    [Added login.jsp]

--- Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/LoginController.java
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/LoginController.java
@@ -2,17 +2,23 @@ package edu.iss.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.iss.inventory.model.User;
 import edu.iss.inventory.service.UserService;
 
 @Controller
-@RequestMapping(value = "/login")
+@RequestMapping(value = "/home")
 public class LoginController {
 	
 	@Autowired
 	UserService userService;
 	
-	
-
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String logic(Model model) {
+		model.addAttribute("user", new User());
+		return "login";
+	}
 }
