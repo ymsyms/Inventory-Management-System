dcdb177c3397982be23db1100babefb4da112a83
ngchongpeng <ngchongpeng@hotmail.com>
2017-12-08 16:45:47 +0800

    Update OrderController

--- Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/OrderController.java
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/controller/OrderController.java
@@ -3,6 +3,7 @@ package edu.iss.inventory.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,53 +18,67 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RequestMapping(value="/order")
+@RequestMapping(value = "/order")
 @Controller
 public class OrderController {
 
 	@Autowired
 	private OrderService oService;
-	
+
 	@Autowired
-	private OrderService oService;
+	private ProductService pService;
 
 	/**
 	 * USER CRUD OPERATIONS
-	 * 
+	 *
 	 * @return
 	 */
 
-	// 
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public ModelAndView newUserPage() {
-		
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ModelAndView newOrder() {
+		ModelAndView mav = new ModelAndView("order");
+		mav.addObject("productList", pService.findInsufficientProduct());
+		return mav;
 	}
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public ModelAndView createNewUser(@ModelAttribute @Valid User user, BindingResult result,
-			final RedirectAttributes redirectAttributes) {
-		
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ModelAndView addProduct(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("order");
+		if(input productId is valid) {
+
+		}
+		else {
+			// go to orderPage and load hashmap
+		}
+		if(got session got order hashmap) {
+			//check if the partId is valid
+			//load data from hashmap
+		}
+		else {
+			//load the data from productService
+		}
+		mav.addObject("product", pService.findProductById());
 	}
 
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView userListPage() {
-		
+
 	}
 
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
 	public ModelAndView editUserPage(@PathVariable String id) {
-		
+
 	}
 
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
 	public ModelAndView editUser(@ModelAttribute @Valid User user, BindingResult result, @PathVariable String id,
 			final RedirectAttributes redirectAttributes) throws UserNotFound {
-		
+
 	}
 
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public ModelAndView deleteUser(@PathVariable String id, final RedirectAttributes redirectAttributes)
 			throws UserNotFound {
-		
+
 	}
 }
