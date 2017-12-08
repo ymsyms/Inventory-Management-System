debbb17345c97f369336079bd2b9c581d1dd4eaa
ymsyms <yimonsoe.yms@gmail.com>
2017-12-08 11:12:02 +0800

    [Added User Repo]

---
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/model/User.java
@@ -0,0 +1,83 @@
+package edu.iss.inventory.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	
+	/** Attributes for Course **/
+	@Id
+	@Column(name = "userId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userId;
+	@Column(name = "userName")
+	private String userName;
+	@Column(name = "password")
+	private String password;
+	@Column(name = "userRole")
+	private String userRole;
+	
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public  User(int userId) {
+		this.userId = userId;
+	}
+
+	public User(int userId, String userName, String password, String userRole) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		this.userRole = userRole;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserRole() {
+		return userRole;
+	}
+
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+}
---
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package edu.iss.inventory.repository;
+
+public interface UserRepository {
+
+}
