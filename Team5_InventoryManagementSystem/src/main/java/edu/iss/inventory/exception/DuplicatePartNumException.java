b22bc0e128c2451fa5945151f8302ef1e1ef75b7
cobsten <cobsten@gmail.com>
2017-12-08 19:23:50 +0800

    Added exception/DuplicatePartNumException.class

---
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/exception/DuplicatePartNumException.java
@@ -0,0 +1,13 @@
+package edu.iss.inventory.exception;
+
+public class DuplicatePartNumException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DuplicatePartNumException() {
+		super();
+	}
+}
