f9b59ff9173b4f555e7c272ba5c0656e8a8f625e
ymsyms <yimonsoe.yms@gmail.com>
2017-12-09 16:24:49 +0800

    Added OrderdetailId.class

--- Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/repository/OrderDetailRepository.java
+++ Team5_InventoryManagementSystem/src/main/java/edu/iss/inventory/repository/OrderDetailRepository.java
@@ -2,7 +2,6 @@ package edu.iss.inventory.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import edu.iss.inventory.model.OrderDetail;
